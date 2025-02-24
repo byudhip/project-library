ba9a34dcca8d4b2890b44d604f94be1c5ac618b1
Yudhi <baskyudh@gmail.com>
2025-02-24 16:04:45 +0700

    Final update
    - Add default library Book prototype inheritor
    - Change "this" usage to "closest" due to unintended behavior

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let myLibrary = [
     title: "wow",
     author: "yudhi",
     year: 1999,
-    pages: 69,
+    pages: 4,
     finishRead: "No",
   },
   {
@@ -35,6 +35,8 @@ let myLibrary = [
   },
 ];
 
+myLibrary = myLibrary.map(book => Object.setPrototypeOf(book, Book.prototype)); //to make sure default books inherit the prototype of Book
+
 function Book(title, author, year, pages, finishRead) {
   (this.title = title),
     (this.author = author),
@@ -54,6 +56,10 @@ function Book(title, author, year, pages, finishRead) {
     });
 }
 
+Book.prototype.toggleReadStatus = function() {
+  this.finishRead = this.finishRead === "Yes" ? "No" : "Yes"
+}
+
 function addBookToLibrary(title, author, year, pages, finishRead) {
   const newBook = new Book(title, author, year, pages, finishRead);
   return myLibrary.push(newBook);
@@ -67,8 +73,9 @@ const refreshDisplay = () => {
       <p>Author: ${book.author}</p>
       <p>Year: ${book.year}</p>
       <p>${book.pages} pages</p>
-      <p class="read-status">Finished reading? ${book.finishRead} </p>
+      <p class="read-status">Finished? ${book.finishRead} </p>
       <button class="toggle-read">Change</button>
+      <p></p>
       <button class="remove-button">Remove</button>
     </div>`;
   }
@@ -97,21 +104,25 @@ newBookModal.addEventListener("click", (e) => {
     );
     console.log(myLibrary);
     refreshDisplay();
-    title.value, author.value, year.value, (pages.value = "");
+    title.value, author.value, year.value, pages.value = "";
     newBookModal.close();
   }
 });
 
   libraryShelf.addEventListener("click", function(e) {
-    const titleToRemove = this.querySelector("h3").textContent.slice(7);
-    const index = myLibrary.findIndex(book => book.title === titleToRemove);
-    const readStatus = this.querySelector(".read-status");
+    const editedBook = e.target.closest(".book"); // to make sure change only happens in interacted book
+    if (!editedBook) return;
+    const titleToRemove = editedBook.querySelector("h3").textContent.slice(7);
+    let index = myLibrary.findIndex(book => book.title === titleToRemove);
+
     if (e.target.classList.contains("remove-button")) {
       myLibrary.splice(index,1);
       refreshDisplay();
-    } else if (e.target.classList.contains(".toggle-read")) {
-      console.log("yes");
-      // myLibrary[index].finishRead = readStatus.textContent === "Yes" ? "No" : "Yes";
-      // refreshDisplay();
+    } else if (e.target.classList.contains("toggle-read")) {
+      console.log("button pressed");
+      console.log(myLibrary[index].title);
+      // myLibrary[index].finishRead = myLibrary[index].finishRead === "Yes" ? "No" : "Yes";
+      myLibrary[index].toggleReadStatus();
+      refreshDisplay();
     }
   });
