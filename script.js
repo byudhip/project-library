939fd6755e4f4f8906166678cf7becfda93f80e8
Yudhi <baskyudh@gmail.com>
2025-02-24 15:13:37 +0700

    Add change and remove button - Add logic to remove button, removing it from array and display - To figure out the change button, which toggle the read status

--- script.js
+++ script.js
@@ -4,7 +4,36 @@ const libraryShelf = document.querySelector(".library-shelf");
 const addBookBtn = document.querySelector(".add-book-btn");
 const newBookModal = document.querySelector(".new-book-modal");
 
-const myLibrary = [];
+let myLibrary = [
+  {
+    title: "wow",
+    author: "yudhi",
+    year: 1999,
+    pages: 69,
+    finishRead: "No",
+  },
+  {
+    title: "nyaharo",
+    author: "sakura miko",
+    year: 2018,
+    pages: 35,
+    finishRead: "Yes",
+  },
+  {
+    title: "kyou mo kawaii",
+    author: "hoshimachi suisei",
+    year: 2018,
+    pages: 125,
+    finishRead: "Yes",
+  },
+  {
+    title: "glasses are versatile",
+    author: "shirakami fubuki",
+    year: 2019,
+    pages: 1277,
+    finishRead: "No",
+  },
+];
 
 function Book(title, author, year, pages, finishRead) {
   (this.title = title),
@@ -30,11 +59,6 @@ function addBookToLibrary(title, author, year, pages, finishRead) {
   return myLibrary.push(newBook);
 }
 
-addBookToLibrary("wow", "yudhi", 1999, 69, "no");
-addBookToLibrary("nyaharo", "sakura miko", 2018, 35, "yes");
-addBookToLibrary("kyou mo kawaii", "hoshimachi suisei", 2018, 125, "yes");
-addBookToLibrary("glasses are versatile", "shirakami fubuki", 2019, 3, "yes");
-
 const refreshDisplay = () => {
   libraryShelf.innerHTML = ``;
   for (let book of myLibrary) {
@@ -43,7 +67,9 @@ const refreshDisplay = () => {
       <p>Author: ${book.author}</p>
       <p>Year: ${book.year}</p>
       <p>${book.pages} pages</p>
-      <p>Finished reading? ${book.finishRead}</p>
+      <p class="read-status">Finished reading? ${book.finishRead} </p>
+      <button class="toggle-read">Change</button>
+      <button class="remove-button">Remove</button>
     </div>`;
   }
 };
@@ -71,9 +97,21 @@ newBookModal.addEventListener("click", (e) => {
     );
     console.log(myLibrary);
     refreshDisplay();
-    title.value, author.value, year.value, pages.value = "";
+    title.value, author.value, year.value, (pages.value = "");
     newBookModal.close();
   }
 });
 
-console.log(myLibrary);
+  libraryShelf.addEventListener("click", function(e) {
+    const titleToRemove = this.querySelector("h3").textContent.slice(7);
+    const index = myLibrary.findIndex(book => book.title === titleToRemove);
+    const readStatus = this.querySelector(".read-status");
+    if (e.target.classList.contains("remove-button")) {
+      myLibrary.splice(index,1);
+      refreshDisplay();
+    } else if (e.target.classList.contains(".toggle-read")) {
+      console.log("yes");
+      // myLibrary[index].finishRead = readStatus.textContent === "Yes" ? "No" : "Yes";
+      // refreshDisplay();
+    }
+  });
