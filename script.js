26c801e1b948a53d711b6e51ae349bb0cefe02fc
Yudhi <baskyudh@gmail.com>
2025-03-06 10:20:34 +0700

    Refactored the code to use classes

--- script.js
+++ script.js
@@ -4,71 +4,57 @@ const libraryShelf = document.querySelector(".library-shelf");
 const addBookBtn = document.querySelector(".add-book-btn");
 const newBookModal = document.querySelector(".new-book-modal");
 
-let myLibrary = [
-  {
-    title: "wow",
-    author: "yudhi",
-    year: 1999,
-    pages: 4,
-    finishRead: "No",
-  },
-  {
-    title: "nyaharo",
-    author: "sakura miko",
-    year: 2018,
-    pages: 35,
-    finishRead: "Yes",
-  },
-  {
-    title: "kyou mo kawaii",
-    author: "hoshimachi suisei",
-    year: 2018,
-    pages: 125,
-    finishRead: "Yes",
-  },
-  {
-    title: "glasses are versatile",
-    author: "shirakami fubuki",
-    year: 2019,
-    pages: 1277,
-    finishRead: "No",
-  },
-];
+class Book {
+  constructor(title, author, year, pages, finishRead) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.pages = pages;
+    this.finishRead = finishRead;
+  }
 
-myLibrary = myLibrary.map(book => Object.setPrototypeOf(book, Book.prototype)); //to make sure default books inherit the prototype of Book
+  toggleReadStatus() {
+    this.finishRead = !this.finishRead;
+  }
+}
 
-function Book(title, author, year, pages, finishRead) {
-  (this.title = title),
-    (this.author = author),
-    (this.year = year),
-    (this.pages = pages),
-    (this.finishRead = finishRead),
-    (this.description = function () {
-      console.log(
-        `${this.title} by ${this.author}, released in ${this.year}, ${
-          this.pages
-        } pages long. ${
-          this.finishRead === "yes"
-            ? "You've read this book."
-            : "You haven't finished this book yet."
-        }`
+class Library {
+  static books = [];
+
+  static getBooks() {
+    return Library.books;
+  }
+  static addBookConsole(...book) {
+    return Library.books.push(...book);
+  }
+
+  static addBookUI(e) {
+    const title = document.querySelector("#title");
+    const author = document.querySelector("#author");
+    const year = document.querySelector("#year");
+    const pages = document.querySelector("#pages");
+    const finishRead = document.querySelector("#finished-reading");
+    if (e.target.classList.contains("cancel-btn")) {
+      newBookModal.close();
+    } else if (e.target.classList.contains("submit-btn")) {
+      e.preventDefault();
+      const newBook = new Book(
+        title.value,
+        author.value,
+        year.value,
+        pages.value,
+        finishRead.value
       );
-    });
-}
-
-Book.prototype.toggleReadStatus = function() {
-  this.finishRead = this.finishRead === "Yes" ? "No" : "Yes"
-}
-
-function addBookToLibrary(title, author, year, pages, finishRead) {
-  const newBook = new Book(title, author, year, pages, finishRead);
-  return myLibrary.push(newBook);
-}
-
-const refreshDisplay = () => {
-  libraryShelf.innerHTML = ``;
-  for (let book of myLibrary) {
-    libraryShelf.innerHTML += `<div class="book">
+      Library.addBookConsole(newBook);
+      console.log(Library.getBooks());
+      Library.refreshDisplay();
+      newBookModal.close();
+    }
+  }
+  static refreshDisplay() {
+    libraryShelf.innerHTML = "";
+    for (let book of Library.books) {
+      libraryShelf.innerHTML += `<div class="book">
       <h3>Title: ${book.title}</h3>
       <p>Author: ${book.author}</p>
       <p>Year: ${book.year}</p>
@@ -78,50 +64,42 @@ const refreshDisplay = () => {
       <p></p>
       <button class="remove-button">Remove</button>
     </div>`;
+    }
   }
-};
-
-refreshDisplay();
-
-addBookBtn.addEventListener("click", () => newBookModal.showModal());
-
-newBookModal.addEventListener("click", (e) => {
-  const title = document.querySelector("#title");
-  const author = document.querySelector("#author");
-  const year = document.querySelector("#year");
-  const pages = document.querySelector("#pages");
-  const finishRead = document.querySelector("#finished-reading");
-  if (e.target.classList.contains("cancel-btn")) {
-    newBookModal.close();
-  } else if (e.target.classList.contains("submit-btn")) {
-    e.preventDefault();
-    addBookToLibrary(
-      title.value,
-      author.value,
-      year.value,
-      pages.value,
-      finishRead.value
-    );
-    console.log(myLibrary);
-    refreshDisplay();
-    title.value, author.value, year.value, pages.value = "";
-    newBookModal.close();
-  }
-});
-
-  libraryShelf.addEventListener("click", function(e) {
+  static shelfEdit(e) {
     const editedBook = e.target.closest(".book"); // to make sure change only happens in interacted book
     if (!editedBook) return;
     const titleToRemove = editedBook.querySelector("h3").textContent.slice(7);
-    let index = myLibrary.findIndex(book => book.title === titleToRemove);
+    let index = Library.books.findIndex((book) => book.title === titleToRemove);
 
     if (e.target.classList.contains("remove-button")) {
-      myLibrary.splice(index,1);
-      refreshDisplay();
+      Library.books.splice(index, 1);
+      Library.refreshDisplay();
     } else if (e.target.classList.contains("toggle-read")) {
       console.log("button pressed");
-      console.log(myLibrary[index].title);
-      myLibrary[index].toggleReadStatus();
-      refreshDisplay();
+      console.log(Library.books[index].title);
+      Library.books[index].toggleReadStatus();
+      Library.refreshDisplay();
     }
-  });
+  }
+}
+
+const book1 = new Book("wow", "yudhi", 1999, 4, false);
+const book2 = new Book("nyaharo", "sakura miko", 2018, 35, true);
+const book3 = new Book("kyou mo kawaii", "hoshimachi suisei", 2018, 125, true);
+const book4 = new Book(
+  "glasses are versatile",
+  "shirakami fubuki",
+  2019,
+  1277,
+  false
+);
+
+Library.addBookConsole(book1, book2, book3, book4);
+Library.refreshDisplay();
+
+addBookBtn.addEventListener("click", () => newBookModal.showModal());
+
+newBookModal.addEventListener("click", Library.addBookUI);
+
+libraryShelf.addEventListener("click", Library.shelfEdit);
